c8cc48e912b3cc3e3c5a0d368dcf1b0dcfe00610
Luis de la Vega <lvega@vendwatchtelematics.com>
2020-08-04 17:32:41 -0500

    Set probe offset to use The Igneus v6 instead of Hero Me gen 5

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -994,7 +994,7 @@
  *
  * Specify a Probe position as { X, Y, Z }
  */
-#define NOZZLE_TO_PROBE_OFFSET { -60, -13, -30 }
+#define NOZZLE_TO_PROBE_OFFSET { -40, -15, -3 }
 
 // Most probes should stay away from the edges of the bed, but
 // with NOZZLE_AS_PROBE this can be negative for a wider probing area.
